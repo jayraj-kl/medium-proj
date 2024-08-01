228c53a1af812490d8ccaa5a0cbb4d0bc04188ef
unknown <jayrajdoingcool9@gmail.com>
2024-08-01 23:04:35 +0530

    Front-end part of the project

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -102,7 +102,18 @@ bookRouter.get('/bulk', async(c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
   try {
-    const post = await prisma.post.findMany()
+    const post = await prisma.post.findMany({
+      select: {
+        content: true,
+        title: true,
+        id: true,
+        author: {
+          select: {
+            name: true
+          }
+        }
+      }
+    })
     return c.json({
       post
     }) 
@@ -123,6 +134,16 @@ bookRouter.get('/:id', async(c) => {
     const post = await prisma.post.findFirst({
       where: {
         id: id
+      },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        author: {
+          select:{
+            name: true
+          }
+        }
       }
     })
     return c.json({
